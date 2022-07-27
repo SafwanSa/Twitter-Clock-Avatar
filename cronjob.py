909ce42c56d67c241db2dc719233637276082fbc
Safwan <s.saigh@puritec.sa>
2022-07-27 20:46:38 +0300

    execute every 5 minutes

--- cronjob.py
+++ cronjob.py
@@ -3,5 +3,5 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from main import cronjob
 
 scheduler = BlockingScheduler()
-scheduler.add_job(cronjob, "interval", seconds=1800)
+scheduler.add_job(cronjob, "interval", seconds=300)
 scheduler.start()
